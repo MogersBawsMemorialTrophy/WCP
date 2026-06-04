d8c2799b9a84ba428084c525bfc45b818fac97e9
MogersBawsMemorialTrophy <tomgill10@googlemail.com>
2026-06-04 16:07:02 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+import { createBracket } from "https://cdn.jsdelivr.net/npm/bracketry/+esm";
+
+const data = {
+  rounds: [
+    { name: "Final" }
+  ],
+
+  matches: [
+    {
+      roundIndex: 0,
+      order: 0,
+      sides: [
+        { contestantId: "eng" },
+        { contestantId: "bra" }
+      ]
+    }
+  ],
+
+  contestants: {
+    eng: {
+      players: [{ title: "England 🏴" }]
+    },
+    bra: {
+      players: [{ title: "Brazil 🇧🇷" }]
+    }
+  }
+};
+
+createBracket(
+  data,
+  document.getElementById("bracket")
+);
